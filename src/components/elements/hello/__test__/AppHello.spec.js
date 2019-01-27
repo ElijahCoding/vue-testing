bf4814cd4743d265b759e27d1b73bef4eba08659
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-27 16:05:03 +0300

    testing props

--- src/components/elements/hello/__test__/AppHello.spec.js
+++ src/components/elements/hello/__test__/AppHello.spec.js
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils'
 import AppHello from '../AppHello'
 
-describe('test', () => {
-    it('works', () => {
+describe('AppHello', () => {
+    it('renders out the hello text', () => {
         let wrapper = mount(AppHello)
 
         expect(wrapper.html()).toContain('Hello')
