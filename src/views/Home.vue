288849c2590685350e47d680be4b0974ab9a6f1c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-28 08:50:34 +0300

    testing classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-      Home
+      <AppTag text="Vue" dark />
   </div>
 </template>
 
 <script>
+import AppTag from '@/components/elements/tag/AppTag'
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+      AppTag
+  }
 }
 </script>
