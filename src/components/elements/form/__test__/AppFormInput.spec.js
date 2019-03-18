e323a52bf007690b8b1218ce714b680744fd4279
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-18 17:36:10 +0300

    building totods

---
+++ src/components/elements/form/__test__/AppFormInput.spec.js
@@ -0,0 +1,6 @@
+import { mount } from '@vue/test-utils'
+import AppFormInput from '../AppFormInput'
+
+describe('AppFormInput', () => {
+
+})
---
+++ src/views/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>bhig</p>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
