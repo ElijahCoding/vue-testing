f988ba49df5a54c5b4a6ab3c66f1d3b8f5d590c8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-18 18:11:55 +0300

    styles

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -2,8 +2,12 @@
     <div class="container">
         <h3>Todos</h3>
         <div class="todos">
-            <div class="todo" v-for="todo in allTodos" :key="todo.id">
-                {{ todo.title }}
+            <div
+              v-for="todo in allTodos"
+              :key="todo.id"
+              class="todo"
+            >
+              {{ todo.title }}
             </div>
         </div>
     </div>
@@ -17,9 +21,11 @@
             this.fetchTodos()
         },
 
-        methods: mapActions([
-            'fetchTodos'
-        ]),
+        methods: {
+            ...mapActions([
+                'fetchTodos'
+            ])
+        },
 
         computed: mapGetters([
             'allTodos'
@@ -38,5 +44,53 @@ body {
   margin: auto;
   overflow: auto;
   padding: 0 2rem;
+}
+
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
