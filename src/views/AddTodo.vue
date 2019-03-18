25ccee546a964838604455722d52ec0af2e426e4
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-18 18:26:22 +0300

    building addTodo

---
+++ src/views/AddTodo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Add Todo</h3>
+    <div class="add">
+      <form @submit="onSubmit">
+        <input type="text" v-model="title" placeholder="Add Todo...">
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                title: ''
+            }
+        },
+
+        methods: {
+            onSubmit (e) {
+                
+            }
+        }
+    }
+</script>
